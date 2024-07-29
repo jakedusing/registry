70d7bcdfd5f9cf350d9793f5b251305e6436726b
Jake Dusing <dusingjake@gmail.com>
2024-07-28 19:00:21 -0500

    add rendering package details

--- src/pages/details/detailsLoader.ts
+++ src/pages/details/detailsLoader.ts
@@ -1,5 +1,27 @@
-export async function detailsLoader() {
-  console.log("hi!");
+import { PackageDetails } from "../../api/types/packageDetails";
+import type { Params } from "react-router-dom";
+import { getPackage } from "../../api/queries/getPackage";
 
-  return "data!";
+interface LoaderArgs {
+  params: Params;
+}
+
+export interface DetailsLoaderResult {
+  details: PackageDetails;
+}
+
+export async function detailsLoader({
+  params,
+}: LoaderArgs): Promise<DetailsLoaderResult> {
+  const { name } = params;
+
+  if (!name) {
+    throw new Error("Name must be provided");
+  }
+
+  const details = await getPackage(name);
+
+  return {
+    details,
+  };
 }
